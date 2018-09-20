c94e18dfc0694eeee1c067219cf0bb24f1156dcc
Weston R16 <mvhs4173@gmail.com>
2018-09-20 16:56:25 -0700

    Started work on Vision processing
    Created "Vision" class where all our vision processing will take place

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -1,7 +1,13 @@
 package org.firstinspires.ftc.teamcode;
-import org.opencv.*;
+import org.opencv.videoio.VideoCapture;
+
+import java.util.concurrent.Executors;
 
 
 public class Vision {
+    VideoCapture videoStream;//The video stream we will be using to get frames from
 
+    public Vision() {
+        videoStream = new VideoCapture();
+    }
 }
