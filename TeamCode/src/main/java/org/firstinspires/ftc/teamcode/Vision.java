afa7b1685749c1523794d5cf8fdb1c3154ab1f16
Weston R16 <mvhs4173@gmail.com>
2018-10-03 18:56:28 -0700

    Vision class can now obtain Camera Ids

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -1,28 +1,92 @@
 package org.firstinspires.ftc.teamcode;
 import android.app.Application;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraCharacteristics;
+
+import org.firstinspires.ftc.robotcore.internal.vuforia.externalprovider.VuforiaWebcam;
+import org.firstinspires.ftc.teamcode.HardwarePushbot;
 
 
 import android.content.Context;
 import android.app.Activity;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.vuforia.Vuforia;
 
 import org.opencv.*;
 
 
 public class Vision {
     CameraManager cameraManager;
-    Context context;
-    /*private String getFrontFacingCameraId(){
-        for(String cameraId : cameraManager.getCameraIdList()){
-            CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
-            int cOrientation = characteristics.get(CameraCharacteristics.LENS_FACING);
-            if(cOrientation == CameraCharacteristics.LENS_FACING_FRONT) return cameraId;
-        }
-        return null;
-    }*/
+    String cameraId;
+    CameraDevice.StateCallback callback;
 
-    public Vision() {
+    //Gets the id of the back camera on the phone
+    private String getBackCameraId(){
+        String[] cameraList = {};
+
+        //Requires a try catch loop to handle errors otherwise the function call will throw an error
+        try {
+           cameraList = cameraManager.getCameraIdList();
+        } catch (CameraAccessException e){
+            e.printStackTrace();
+        }
+
+        //Loop through the list of camera ids
+        for (String cameraId : cameraList) {
+            CameraCharacteristics camCharacteristics = null;
+
+            try {
+                camCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+
+            int camOrientation = camCharacteristics.get(CameraCharacteristics.LENS_FACING);
+            if (camOrientation == CameraCharacteristics.LENS_FACING_BACK) {
+                return cameraId;
+            }
+        }
+
+        return null;
+    }
+
+    //Gets the id of the front camera on the phone
+    private String getFrontCameraId(){
+        String[] cameraList = {};
+
+        //Requires a try catch loop to handle errors otherwise the function call will throw an error
+        try {
+            cameraList = cameraManager.getCameraIdList();
+        } catch (CameraAccessException e){
+            e.printStackTrace();
+        }
+
+        //Loop through the list of camera ids
+        for (String cameraId : cameraList) {
+            CameraCharacteristics camCharacteristics = null;
+
+            try {
+                camCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+
+            int camOrientation = camCharacteristics.get(CameraCharacteristics.LENS_FACING);
+            if (camOrientation == CameraCharacteristics.LENS_FACING_FRONT) {
+                return cameraId;
+            }
+        }
+
+        return null;
+    }
+
+    public Vision(HardwareMap hwMap) {
+        //Get the camera manager
+        cameraManager = (CameraManager)hwMap.appContext.getSystemService(Context.CAMERA_SERVICE);
+        cameraId = getBackCameraId();//Get the id of the camera we will be using
 
     }
 }
