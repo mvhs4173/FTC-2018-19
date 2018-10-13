a1c71239ae93179607c586a26c3e8e843e95b669
R15 <mvhs4173@gmail.com>
2018-10-13 12:34:03 -0700

    Zach 10/11/18
    Worked with the Collector subsystem,
    renamed some objects

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Collector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Collector.java
@@ -2,26 +2,25 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class Collector {
-    private DcMotor collectorMotor;
-    private DcMotor collectorMotor2;
+public class CollectorArm {
+    private DcMotor elbowMotor;
 
-    public Collector(DcMotor collectorMotor,
+
+    public CollectorArm(DcMotor collectorMotor,
                      DcMotor collectorMotor2) {
-        this.collectorMotor = collectorMotor;
-        this.collectorMotor2 = collectorMotor2;
+        this.elbowMotor = collectorMotor;
+
     }
 
-    public void Intake(double inputPower) {
-        collectorMotor.setPower(inputPower);
-    }
-
-    public void Arm(double inputPower) {
-        collectorMotor2.setPower(inputPower);
+    public void ArmMotor(double inputPower) {
+        elbowMotor.setPower(inputPower);
     }
 
     public void stopMotor() {
-        collectorMotor.setPower(0);
-        collectorMotor2.setPower(0);
+        elbowMotor.setPower(0);
     }
+    public void setElbowMotor() {
+        elbowMotor.setTargetPosition();
+    }
+
 }
