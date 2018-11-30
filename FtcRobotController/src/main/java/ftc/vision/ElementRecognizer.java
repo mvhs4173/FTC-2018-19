df33e0faa662f0d1d92fe060c60db20e3695410b
Weston R16 <mvhs4173@gmail.com>
2018-11-29 18:14:29 -0800

    Add Derivative to PD loop

--- FtcRobotController/src/main/java/ftc/vision/ElementRecognizer.java
+++ FtcRobotController/src/main/java/ftc/vision/ElementRecognizer.java
@@ -28,7 +28,11 @@ public class ElementRecognizer {
 
     }
 
-
+    Point lastCubePosition = new Point(-1, -1);
+    double lastCubeArea = 0.0;
+    MatOfPoint lastContour = null;
+    double lastCubeWidth = 0.0;
+    double lastCubeHeight = 0.0;
 
     public ObjectDetectionResult yellowCubeFilter(Mat image, ScreenOrientation orientation, Point screenDimensions) {
         Mat cannyImage = new Mat();
@@ -41,6 +45,7 @@ public class ElementRecognizer {
         Mat outImage = new Mat();
 
         Point cubePosition = new Point(-1, -1);
+
 
         Size cubeSize = new Size(0, 0);
 
@@ -57,7 +62,7 @@ public class ElementRecognizer {
 
             //Color values to filter for in HSV format
             Scalar maxRange = new Scalar(109, 255, 255);
-            Scalar lowestRange = new Scalar(72, 170, 145);
+            Scalar lowestRange = new Scalar(72, 160, 145);
 
             Core.inRange(hsv, lowestRange, maxRange, mask);//Get only the pixels in the correct color range
 
@@ -83,7 +88,7 @@ public class ElementRecognizer {
                 double area = Imgproc.contourArea(currentContour);
 
                 //Make sure its in a basic square/cube shape
-                if (area >= 30) {
+                if (area >= 5) {
                     //Get a bounding box
                     Rect positionBox = Imgproc.boundingRect(currentContour);
                     Point boxPosition = new Point(positionBox.x, positionBox.y);
@@ -111,10 +116,35 @@ public class ElementRecognizer {
                 cubeSize.height = boundingBox.height;
                 cubeSize.width = boundingBox.width;
 
+                cubePosition = new Point(boundingBox.x + (boundingBox.width/2), boundingBox.y + (boundingBox.height/2));
+
+                //Check if the new cube position is within the position of the last one
+                if (lastCubePosition.x != -1) {
+                    //If the cube is within the area of the last cube on the X axis
+                    if (Math.abs(cubePosition.x - lastCubePosition.x) < lastCubeWidth/2) {
+                        //If the cube is within the area of the last cube on the Y axis
+                        if (Math.abs(cubePosition.y - lastCubePosition.y) < lastCubeHeight/2) {
+                            //Now if the area of the last cube is larger than the current one then use the last cube
+                            if (lastCubeArea > cubeSize.height * cubeSize.width) {
+                                //Use the last cube position and box
+                                cubeSize.height = lastCubeHeight;
+                                cubeSize.width = lastCubeWidth;
+                                boundingBox = Imgproc.boundingRect(lastContour);
+                                cubePosition = lastCubePosition;
+                            }
+                        }
+                    }
+                }
+
                 Imgproc.rectangle(image, new Point(boundingBox.x, boundingBox.y), new Point(boundingBox.x + boundingBox.width, boundingBox.y + boundingBox.height), new Scalar(0, 255, 0));
 
                 //The position of the cube in the image
-                cubePosition = new Point(boundingBox.x + (boundingBox.width/2), boundingBox.y + (boundingBox.height/2));
+
+                lastCubePosition = cubePosition;
+                lastCubeArea = cubeSize.height * cubeSize.width;
+                lastCubeWidth = cubeSize.width;
+                lastCubeHeight = cubeSize.height;
+                lastContour = targetContours.get(largestIndex);
             }
             //Imgproc.drawContours(image, contours, largestIndex, new Scalar(0, 255, 0), 3);
             //Rect boundingBox = Imgproc.boundingRect((MatOfPoint)contour);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CubeAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CubeAuto.java
@@ -12,16 +12,21 @@ import ftc.vision.ObjectDetectionResult;
 
 @Autonomous(name = "CubeAuto", group = "Auto")
 public class CubeAuto extends OpMode {
-    //Objects
+
     Hardware hardware = new Hardware();
     DriveTrain driveTrain;
-    FrameGrabber vision;
+    FrameGrabber vision;//The thing that gets the next frame from the camera
     Point lastObjectPosition = new Point(-1, -1);
     double lastDetectionTime = 0.0;//The last time that the camera detected an object in the frame
     Timer timer;
 
+    //PID//
+    double lastLoopTime = 0.0;//The time of last loop()
     double allowedError = 5.0;
-    double pFactor = 0.008;
+    double pFactor = 0.003;
+    double dFactor = 0.5;
+    double lastError = 0.0;
+    ////////
 
     Point screenDimensions;
 
@@ -38,36 +43,37 @@ public class CubeAuto extends OpMode {
 
     @Override
     public void init_loop() {
-        //hardware.compass.resetHeading();
-
+        //Wait until vision is initialized
         if (vision != null) {
             vision.setDetectionMode(DetectionMode.GOLD_CUBE);//Tell the camera to look for gold cube
-            vision.setScreenOrientation(FrameGrabber.ScreenOrientation.PORTRAIT);//Tell the camera we want landscape mode
+            vision.setScreenOrientation(FrameGrabber.ScreenOrientation.PORTRAIT);//Tell the camera we want portrait mode
 
         }
 
-        //telemetry.addData("Raw Compass", hardware.compass.getRawHeading());
-        //telemetry.addData("heading", hardware.compass.getHeading());
-        //telemetry.addData("OriginAngle", hardware.compass.getOriginalRawHeading());
-
-        telemetry.update();
     }
 
-    boolean aligned = false;
+    boolean aligned = false;//Made true after the robot has aligned itself with golden cube
     double pixelPerInch = 13.0/30.0;//13 pixels divided by 30 inches
-    double distanceToCube = -1.0;
+    double distanceToCube = -1.0;//-1 meaning we have not calculated a distance
 
-    double leftOriginDist = 0.0;
+    double leftOriginDist = 0.0;//How far the left motor has traveled used as a relative point so that we can 'reset' encoders
     double rightOriginDist = 0.0;
+
+    int numTimesAligned = 0;//The number of iterations the robot is lined up with the cube this is to make sure it doesnt overshoot when we cut power to the motors
+
+
 
     @Override
     public void loop() {
+        double currentLoopTime = System.currentTimeMillis()/1000;//The current time of the loop in seconds since unix epoch
+        double currentError = 0.0;
+
+
         screenDimensions = vision.getScreenDimensions();
-        //telemetry.addData("Cube Height", vision.lastHeight);
         double currentTimeSeconds = System.currentTimeMillis()/1000;//Current time, in seconds, since unix epoch
         //Make sure vision has been initialized
         if (vision != null) {
-            boolean usedLastPosition = false;
+            boolean usedLastPosition = false;//Say that we have not used the position found in last frame
 
             //Get information that the camera found
             ObjectDetectionResult processingResult = vision.getLastProcessingResult();//Get the information about the last analyzed frame
@@ -76,7 +82,7 @@ public class CubeAuto extends OpMode {
             //If the camera could not find the cube in this frame then check how long ago we had a position
             if (cubePosition.x == -1 && currentTimeSeconds - lastDetectionTime <= 0.3) {
                 cubePosition= lastObjectPosition;
-                usedLastPosition = true;
+                usedLastPosition = true;//Say we are using the position found in the last frame
             }
 
             //Dont update if we didnt find the cube in this frame
@@ -88,19 +94,36 @@ public class CubeAuto extends OpMode {
             double targetPosition = screenDimensions.x/2;
 
             //Make sure the cube is in view of the camera
-            if (cubePosition.x > -1) {
-                double currentError = targetPosition - cubePosition.x;//How far away the robot is from the target
+            if (cubePosition.x > -1 && !aligned) {
+                currentError = targetPosition - cubePosition.x;//How far away the robot is from the target
+
+                //Calculate the derivative part of PD loop
+                double derivativeNumber = ((currentError - lastError)/(currentLoopTime - lastLoopTime)) * dFactor;
+                //Calculate the proportional part of PD loop
+                double proportionalNumber = currentError * pFactor;
+
+                newSpeed = derivativeNumber + proportionalNumber;//Calculate the new speed for the motors
 
                 //If the robot is not lined up
                 if (Math.abs(currentError) > allowedError) {
-                    newSpeed = currentError * pFactor;
+                        driveTrain.left.setPower(newSpeed);
+                        driveTrain.right.setPower(-newSpeed);
                 } else {
-                    aligned = true;
+                    numTimesAligned++;
+
+                    //If the robot has been aligned for the past 20 iterations then say its finished
+                    //This is to prevent overshoot when we cut power to the motor
+                    if (numTimesAligned >= 20) {
+                        aligned = true;//Say the robot is aligned and stop the motors
+                        driveTrain.left.setPower(0.0);
+                        driveTrain.right.setPower(0.0);
+                    }
                 }
             }
 
-            //Make the robot turn in place to align itself with the target
+            //If the robot has not been aligned with the cube yet
             if (!aligned) {
+                //Turn in place
                 driveTrain.left.setPower(newSpeed);
                 driveTrain.right.setPower(-newSpeed);
                 distanceToCube = processingResult.getObjectSize().height/pixelPerInch + 3.0;//Estimates roughly how far away the cube is
@@ -109,7 +132,7 @@ public class CubeAuto extends OpMode {
             }
 
             //If the robot is aligned with the cube prepare to move forward
-            if (aligned) {
+            /*if (aligned) {
                 double inchesDriven = ((driveTrain.getLeftInchesDriven() - leftOriginDist) + (driveTrain.getRightInchesDriven() - rightOriginDist))/2;
 
                 if (inchesDriven < distanceToCube) {
@@ -122,10 +145,12 @@ public class CubeAuto extends OpMode {
                 telemetry.addData("Inches to cube", distanceToCube);
                 telemetry.addData("Inches driven", inchesDriven);
 
-            }
+            }*/
 
-
-
+            lastLoopTime = System.currentTimeMillis()/1000;
+            lastError = currentError;
+            telemetry.addData("Right Speed", driveTrain.right.getPower());
+            telemetry.addData("Left Speed", driveTrain.left.getPower());
             telemetry.addData("Cube Aligned: ", aligned);
             telemetry.addData("Turn speed", newSpeed);
             telemetry.addData("Cube Position:", String.valueOf(cubePosition.x) + ", " + String.valueOf(cubePosition.y));
